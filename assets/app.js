e3967db06e01098ecfabe906d08d8bc43e76f68e
Omar Abdelhadi <69729443+omarabdelhadi129@users.noreply.github.com>
2022-01-03 21:39:48 -0500

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,26 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".nav_menu");
+const navLogo = document.querySelector("#nav_logo");
+
+const lang1 = document.getElementById("lang1");
+
+// function to hide mobile menu 
+function hideMobile() {
+    const menuBar = document.querySelector(".is-active");
+
+    if (window.innerWidth <= 960 && menuBar) {
+        menu.classList.toggle("is-active");
+        menuLinks.classList.remove("active");
+    }
+}
+
+// Adding the event listner for mobile menu view 
+menu.addEventListener("click", function () {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+});
+
+
+// Adding event listners to remove mobile 
+menuLinks.addEventListener("click", hideMobile);
+navLogo.addEventListener("click", hideMobile);
